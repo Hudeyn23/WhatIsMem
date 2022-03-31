e16f9a65b7b8bb1450ce785e389f8b1f427cc3a6
Hudeyn <alexskite25@gmail.com>
2022-04-01 00:06:56 +0700

    init backend commit

---
+++ backend/src/main/java/com/nsu/whatismem/WhatismemApplication.java
@@ -0,0 +1,13 @@
+package com.nsu.whatismem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatismemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatismemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nsu/whatismem/WhatismemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nsu.whatismem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatismemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
