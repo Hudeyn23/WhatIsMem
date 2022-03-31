56acacc41468969fa6ebc2e71dc77530fd8c150c
Hudeyn <alexskite25@gmail.com>
2022-04-01 00:28:16 +0700

    add frontend and backend init

--- backend/src/test/java/com/nsu/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/nsu/backend/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package com.nsu.whatismem;
+package com.nsu.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WhatismemApplicationTests {
+class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
