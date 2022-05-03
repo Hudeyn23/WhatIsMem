f456061e2d560d38c7a7fd8ccbb2fb647530752d
Hudeyn <alexskite25@gmail.com>
2022-05-03 16:17:20 +0700

    change room code format

--- backend/src/main/java/com/nsu/backend/RoomService.java
+++ backend/src/main/java/com/nsu/backend/RoomService.java
@@ -10,6 +10,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class RoomService {
+    private final int max = 9999;
+    private final int min = 1000;
     private ConcurrentHashMap<String, Room> rooms = new ConcurrentHashMap<>();
     private final Set<String> roomsSet = ConcurrentHashMap.newKeySet();
     private final Random random = new Random();
@@ -17,7 +19,7 @@ public class RoomService {
     public Room createRoom(int players) {
         String id;
         do {
-            id = String.format("%04d", random.nextInt(1000,10000));
+            id = String.format("%04d", random.nextInt((max - min) + min));
         } while (!roomsSet.add(id));
         Room room = new Room(id, players);
         rooms.put(id, room);
